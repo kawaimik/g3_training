ee2d436d633963d132a86e4053166b43cba37b5b
kawaimik <165769730+kawaimik@users.noreply.github.com>
2025-03-05 18:00:27 +0900

    [AL00602(Kawai Miki) on 20250305090026]

---
+++ src/views/test_01.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { useLockScreen } from 'viy-ui';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+const { lockScreen } = useLockScreen();
+
+defineOptions({
+  name: 'test_01',
+});
+
+const form = ref();
+
+const formData = reactive({
+});
+</script>
+
+<template>
+  <VueForm ref="form" v-loading="lockScreen" :model="formData">
+    <VueText id="viy2Text_baWYq" ref="viy2Text_baWYq" :style="{ width: '100%', display: 'inline-block' }">
+      Hello World
+    </VueText>
+  </VueForm>
+</template>
